f9d5b6873e0d1bcef8c6d83281a263b3ca65f0e9
Nicolas Cardozo <36283346+Nicolas-Cardozo@users.noreply.github.com>
2018-05-06 22:13:59 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -17,7 +17,7 @@ class Candidato(models.Model):
     nome = models.CharField(max_length=200)
     email = models.CharField(max_length=50)
     
-class localprova(models.Model):
+class Localprova(models.Model):
     instituicao = models.CharField(max_length=200)
     local = models.CharField(max_length=200)
     data = models.CharField(max_length=50)
